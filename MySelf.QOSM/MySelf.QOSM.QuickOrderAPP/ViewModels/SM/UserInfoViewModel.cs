064095af7866ddb904e04c9e0ac5d765e8c7089f
何正方 <hezhengfang08@163.com>
2024-06-04 23:23:25 +0800

    角色用户添加

---
+++ MySelf.QOSM/MySelf.QOSM.QuickOrderAPP/ViewModels/SM/UserInfoViewModel.cs
@@ -0,0 +1,228 @@
+﻿using MySelf.QOSM.BLLFactory;
+using MySelf.QOSM.IServices;
+using MySelf.QOSM.Models.Entities;
+using MySelf.QOSM.Models.UIModels;
+using MySelf.QOSM.QuickOrderAPP.Utils;
+using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MySelf.QOSM.QuickOrderAPP.ViewModels.SM
+{
+    public class UserInfoViewModel:InfoVMBase<ViewUserInfo>
+    {
+        IRoleService roleService = InstanceFactory.CreateInstance<IRoleService>();
+        IUserService userService = InstanceFactory.CreateInstance<IUserService>();
+        UserInfo userInfo;
+        string oldName = "";
+        public UserInfoViewModel (int actType, ViewUserInfo vuser)
+        {
+            ActType = actType;
+            ShowUNameError = Visibility.Hidden;
+            ShowTime = Visibility.Collapsed;
+            LoadCboRoles();
+            if(ActType == 1)
+            {
+                userInfo = new UserInfo();
+                IsRequired = true;
+                IsPRequired = true;
+                RoleId = 0;
+                UserState = true;
+                SubmitText = "新增";
+            }
+            else
+            {
+                userInfo = new UserInfo()
+                {
+                    UserId = vuser.UserId,
+                    UserName = vuser.UserName,
+                    RoleId = vuser.RoleId,
+                    Phone = vuser.Phone,
+                    CreateTime = vuser.CreateTime,
+                    UserState = vuser.UserState
+                };
+                oldName = vuser.UserName;
+                ShowTime = Visibility.Visible;
+                SubmitText = "修改";
+            }
+            //命令初始化
+            InitCommands();
+
+        }
+        #region 属性
+        private bool isRequired;
+
+        public bool IsRequired
+        {
+            get { return isRequired; }
+            set { isRequired = value;
+                OnPropertyChanged();
+            }
+        }
+        private Visibility showUNameError;
+
+        public Visibility ShowUNameError
+        {
+            get { return showUNameError; }
+            set { showUNameError = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int UserId
+        {
+            get { return userInfo.UserId; }
+            set
+            {
+                userInfo.UserId = value;
+            }
+        }
+        //用户名
+        public string UserName
+        {
+            get { return userInfo.UserName; }
+            set { userInfo.UserName = value; }
+        }
+        //用户密码
+        public string UserPwd
+        {
+            get { return userInfo.UserPwd; }
+            set { userInfo.UserPwd = value; }
+        }
+        private bool  isPRequired;
+
+        public bool IsPRequired
+        {
+            get { return isPRequired; }
+            set { isPRequired = value;
+                OnPropertyChanged();
+            }
+        }
+        private List<UIRole> roleList;
+
+        public List<UIRole> RoleList
+        {
+            get { return roleList; }
+            set { roleList = value; }
+        }
+        private int roleId;
+
+        public int RoleId
+        {
+            get { return roleId; }
+            set { roleId = value; }
+        }
+
+        //手机号码
+        public string Phone
+        {
+            get { return userInfo.Phone; }
+            set { userInfo.Phone = value; }
+        }
+        //创建时间
+        public string CreateTime
+        {
+            get
+            {
+                if (userInfo.CreateTime != DateTime.MinValue)
+                    return userInfo.CreateTime.ToString("yyyy-MM-dd HH:mm:ss");
+                else
+                    return null;
+            }
+        }
+
+        private Visibility showTime;
+
+        public Visibility ShowTime
+        {
+            get { return showTime; }
+            set { showTime = value; }
+        }
+        //用户状态
+        public bool UserState
+        {
+            get { return userInfo.UserState; }
+            set { userInfo.UserState = value; }
+        }
+
+        #endregion
+        #region 方法
+        private void LoadCboRoles()
+        {
+            List<UIRole> roles = roleService.GetCboRoles();
+            roles.Insert(0, new UIRole() { RoleId = 0, RoleName = "请选择" });
+            RoleList = roles;
+        }
+
+        private void SaveUser(object win)
+        {
+            string conType = ActType == 1? "新增":"修改";
+            string msgTitle = $"用户{conType}";
+            if (string.IsNullOrEmpty(UserName))
+            {
+                IsRequired = true;
+                if (ShowUNameError == Visibility.Collapsed)
+                    ShowUNameError = Visibility.Visible;
+                return;
+            }
+            if(ActType == 1 ||(ActType ==2 && UserName != oldName) ){
+                if (userService.ExistUser(UserName))
+                {
+                    ShowUNameError = Visibility.Visible;
+                    return;
+                }
+            }
+            if(string.IsNullOrEmpty(UserPwd) && ActType == 1)
+            {
+                IsPRequired = true;
+                return;
+            }
+            if(RoleId == 0)
+            {
+                ShowError("请设置用户角色");
+                return;
+            }
+            bool blConfirm = userService.SaveUser(userInfo);
+            if (blConfirm)
+            {
+                ShowSuccess($"用户：{UserName} {conType}成功！", msgTitle);
+                //刷新
+                ViewUserInfo vuser = new ViewUserInfo()
+                {
+                    UserId = userInfo.UserId,
+                    UserName = userInfo.UserName,
+                    RoleId = userInfo.RoleId,
+                    RoleName = RoleList.Find(r => r.RoleId == RoleId).RoleName,
+                    Phone = userInfo.Phone,
+                    CreateTime = userInfo.CreateTime,
+                    UserState = userInfo.UserState
+                };
+                OnReloadList(vuser);
+                //关闭窗口
+                CloseWindow(win);
+            }
+            else
+            {
+                ShowError($"用户：{UserName} {conType}失败！", msgTitle);
+                return;
+            }
+        }
+        private void InitCommands()
+        {
+            MoveWindowCmd = CreateMoveWindowCmd();
+            CloseWindowCmd = CreateCloseWindowCmd();
+            ConfirmCmd = new RelayCommand(win =>
+            {
+                SaveUser(win);
+            });
+        }
+        #endregion
+    }
+}
---
+++ MySelf.QOSM/MySelf.QOSM.QuickOrderAPP/ViewModels/SM/UserListViewModel.cs
@@ -0,0 +1,211 @@
+﻿using MySelf.QOSM.BLLFactory;
+using MySelf.QOSM.IServices;
+using MySelf.QOSM.Models.Entities;
+using MySelf.QOSM.Models.UIModels;
+using MySelf.QOSM.QuickOrderAPP.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace MySelf.QOSM.QuickOrderAPP.ViewModels.SM
+{
+    public class UserListViewModel:ListVMBase
+    {
+        bool oldShowDel = false;
+        IUserService userService = InstanceFactory.CreateInstance<IUserService>();
+        public UserListViewModel()
+        {
+            ShowDeleted = true;
+            ShowDeleted = false;
+            PageInofVM = new UControl.UPagerViewModel();
+            PageInofVM.PageSize = 10;
+            PageInofVM.PageChanged += (o, e) => LoadUserList();
+            LoadUserList();
+            InitCommands();
+
+
+        }
+        #region 属性
+        private ObservableCollection<UserModel> userList;
+        public ObservableCollection<UserModel> UserList
+        {
+            get {
+                return userList;
+            }
+
+            set
+            {
+                userList =value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+        #region 命令
+        public ICommand EnableUser { get; set; }
+        #endregion
+        #region 方法
+       private void LoadUserList()
+        {
+            ObservableCollection<UserModel> users = new ObservableCollection<UserModel>();
+            if(HasDeleted != oldShowDel)
+            {
+                PageInofVM.CurrentPage = 1;
+                oldShowDel = HasDeleted;
+            }
+            PageModel<ViewUserInfo> res = userService.GetUserList(KeyWords, HasDeleted, PageInofVM.StartIndex, PageInofVM.PageSize);
+            if(res != null) {
+                List<ViewUserInfo> userData = null;
+                if (res.DataList.Count > 0)
+                {
+                    userData = res.DataList;
+                    PageInofVM.TotalCount = res.TotalCount;
+                    userData.ForEach( u=> users.Add(new UserModel(u)));
+                }
+                else
+                {
+                    PageInofVM.TotalCount =0 ;
+                }
+            }
+            UserList = users;
+        }
+        private void ShowUserWindow(int actType, ViewUserInfo user)
+        {
+            UserInfoViewModel userVM = new UserInfoViewModel(actType, user);
+            userVM.ReloadList += UserVM_ReLoadList;
+            ShowDialog("userWindow", userVM);
+        }
+        private void UserVM_ReLoadList(object sender, InfoArgs<ViewUserInfo> e)
+        {
+            if(e != null)
+            {
+                if(e.ActType == 1 && UserList.Count < PageInofVM.PageCount) {
+                    UserList.Add(new UserModel(e.Info));
+                }
+                else if(e.ActType == 2)
+                {
+                    var userModel = UserList.Where(u=>u.User.UserId == e.Info.UserId).FirstOrDefault();
+                    userModel.User = e.Info;//替换
+                    if(userModel.ShowEnable == Visibility.Visible)
+                    {
+                        userModel.EnableText = e.Info.UserState ? "启用" : "停用";
+                    }
+                }
+            }
+        }
+        private void SetUserState(UserModel model)
+        {
+            var user = model.User;
+            string enableType = user.UserState ? "启用" : "停用";
+            string msgTitle = $"{enableType}用户";
+            if(ShowQuestion($"确定要{enableType}该用户吗？",msgTitle)==MessageBoxResult.OK)
+            {
+                bool setState = user.UserState ? false : true;
+                bool lbSet = false;
+                lbSet = userService.SetUserState(user.UserId, setState);
+                if (lbSet)
+                {
+                    ShowSuccess($"用户：{user.UserName} {enableType}成功！", msgTitle);
+                    //刷新
+                    user.UserState = setState;
+                    model.EnableText = setState ? "停用" : "启用";
+                    model.User = user;
+                }
+                else
+                {
+                    ShowError($"用户：{user.UserName} {enableType}失败！", msgTitle);
+                    return;
+                }
+            }
+        }
+        private  void InitCommands()
+        {
+            AddItemCmd = new RelayCommand(
+                win =>
+                {
+                    ShowUserWindow(1, null);
+
+                });
+            EditItemCmd = new RelayCommand(
+                u =>
+                {
+                    ViewUserInfo viewUserInfo = u as ViewUserInfo;
+                    ShowUserWindow(2, viewUserInfo);
+                });
+            FindDataListCmd = new RelayCommand(
+                o =>
+                {
+                    LoadUserList();
+                });
+            EnableUser = new RelayCommand(
+                o =>
+                {
+                    UserModel user = o as UserModel;
+                    SetUserState(user);
+                });
+            DelItemCmd = new RelayCommand(u =>
+            {
+                UserModel model = u as UserModel;
+                DeleteUser(model, 1);
+            });
+            RecoverItemCmd = new RelayCommand(u =>
+            {
+                UserModel model = u as UserModel;
+                DeleteUser(model, 2);
+            });
+            RemoveItemCmd = new RelayCommand(u =>
+            {
+                UserModel model = u as UserModel;
+                DeleteUser(model, 3);
+            });
+        }
+        private void DeleteUser(UserModel model,int delType)
+        {
+            string info = "用户";
+            string delName = CommonHelper.GetDelTypeName(delType);
+            string msgTitle = $"{info}{delName}";
+            if(ShowQuestion($"你确定要{delName}该用户吗？",msgTitle)  == MessageBoxResult.OK) { 
+                ViewUserInfo user = model.User;
+                bool blResult = false;
+                switch(delType)
+                {
+                    case 1:
+                        blResult = userService.DeleteUser(user.UserId);
+                        break;
+                        case 2:
+                        if (userService.ExistUser(user.UserName))
+                        {
+                            ShowError("已存在与该账号相同的用户，不能恢复！", msgTitle);
+                            return;
+                        }
+                        blResult = userService.RecoverUser(user.UserId);
+                        break;
+                    case 3:
+                        blResult = userService.RemoveUser(user.UserId);
+                        break;
+                    default: break;
+                }
+                if (blResult)
+                {
+                    ShowSuccess($"该{info}{delName}成功！", msgTitle);
+                    //刷新---用户信息从当前列表中移除
+                   if(delType != 2) UserList.Remove(model);
+                }
+                else
+                {
+                    ShowError($"该{info}{delName}失败！", msgTitle);
+                    return;
+                }
+
+            }
+
+        }
+        #endregion
+    }
+}
