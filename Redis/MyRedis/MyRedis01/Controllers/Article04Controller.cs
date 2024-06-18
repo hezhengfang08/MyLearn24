38d5acdb3dbab15476d15c9c89203abc8f702e2c
何正方 <hezhengfang08@163.com>
2024-06-18 09:55:02 +0800

    set hast string list

---
+++ Redis/MyRedis/MyRedis01/Controllers/Article04Controller.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRedis01.Dtos;
+using MyRedis01.Entities;
+using MyRedis01.RedisConstants;
+using MyRedis01.RequestFeatures;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using System.Globalization;
+
+namespace MyRedis01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Article04Controller : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IRedisDatabase redis;
+        public Article04Controller(IMapper mapper, IRedisDatabase redis)
+        {
+            this.mapper = mapper;
+            this.redis = redis;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetAsync(long id)
+        {
+
+            var article = await redis.HashGetAllAsync<object>(ArticleConstant.ArticleData + id);
+            var pageView = await redis.SortedSetAddIncrementAsync(ArticleConstant.ArticlesPageView, id, 1);
+            article.Add("PageView", pageView);
+            return Ok(article);
+
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateArticleDto createArticle)
+        {
+            var article = mapper.Map<Article>(createArticle);
+            article.Id = await redis.Database.StringIncrementAsync(ArticleConstant.ArticleCount);
+            article.ReleaseTime = DateTime.Now.ToString(CultureInfo.CurrentCulture);
+            var hashKey = ArticleConstant.ArticleData + article.Id;
+            var articleDic = mapper.Map<Dictionary<string, object>>(article);
+            await redis.HashSetAsync(hashKey, articleDic);
+            await redis.ListAddToLeftAsync(ArticleConstant.ArticleList, article.Id);
+            return CreatedAtAction("Get", new { id = article.Id, article });
+
+        }
+        [HttpDelete]
+        public async Task<ActionResult> Delete(long id)
+        {
+            {
+                await redis.RemoveAsync(ArticleConstant.ArticleData + id);
+                await redis.RemoveAsync(ArticleConstant.ArticlePageView + id);
+                await redis.Database.ListRemoveAsync(ArticleConstant.ArticleList, id);
+                await redis.Database.StringDecrementAsync(ArticleConstant.ArticleCount);
+                await redis.SetRemoveAsync(ArticleConstant.ArticleTags, id);
+                await redis.SortedSetRemoveAsync(ArticleConstant.ArticlesPageView, id);
+
+                return Ok();
+
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetArticles(ArticleParameter parameter)
+        {
+            var start = (parameter.PageNumber - 1) * parameter.PageSize + 1;
+            var end = parameter.PageNumber * parameter.PageSize;
+            var ids = await redis.SortedSetRangeByRankWithScoresAsync<long>(ArticleConstant.ArticlesPageView, start, end);
+            var result = new List<IDictionary<string, object>>();
+            foreach (var id in ids)
+            {
+                var temp = await redis.HashGetAllAsync<object?>(ArticleConstant.ArticleData + id);
+                result.Add(temp);
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ Redis/MyRedis/MyRedis01/Controllers/TagController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using MyRedis01.RedisConstants;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
+
+namespace MyRedis01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly IRedisDatabase redis;
+        public TagController(IRedisDatabase redis)
+        {
+            this.redis = redis;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetTags(long id)
+        {
+            var result = await redis.SetMemberAsync(ArticleConstant.ArticleTags + id);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<ActionResult> CreateTags(long id, string tags)
+        {
+           await redis.SetAddAllAsync(ArticleConstant.ArticleTags, StackExchange.Redis.CommandFlags.None,tags.Split(","));
+            return Ok();
+           
+           
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DelteTag(long id, string tag)
+        {
+            await redis.SetRemoveAsync(ArticleConstant.ArticleTags+id, tag);
+            return Ok();    
+        }
+
+    }
+}
