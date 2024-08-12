57f90dc4cff6ff80e063cf175ae5bf0c745a1faf
何正方 <hezhengfang08@163.com>
2024-08-12 10:28:53 +0800

    下载文件的接口

--- MyWPF/MySelf.PMS.Server/MySelf.PMS.Server.Entities/SysEmployee.cs
+++ MyWPF/MySelf.PMS.Server/MySelf.PMS.Server.Entities/SysEmployee.cs
@@ -1,7 +1,45 @@
-﻿namespace MySelf.PMS.Server.Entities
+﻿using SqlSugar;
+namespace MySelf.PMS.Server.Entities
 {
-    public class Class1
+    public class SysEmployee
     {
+        [SugarColumn(IsPrimaryKey = true)]
+        public int EId { get; set; }
 
+        public string UserName { get; set; }
+
+        public string RealName { get; set; }
+
+        public string Password { get; set; }
+
+        public int Status { get; set; }
+
+        public int Age { get; set; }
+
+        public int Gender { get; set; }
+
+        public string EIcon { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string QQ { get; set; }
+
+        public string WeChat { get; set; }
+
+        public string LastLoginTime { get; set; }
+
+        public string CreateTime { get; set; }
+
+        public int CreateId { get; set; }
+
+        public string LastModifyTime { get; set; }
+
+        public int LastModifyId { get; set; }
     }
 }
--- MyWPF/MySelf.PMS.Server/MySelf.PMS.Server.Start/Program.cs
+++ MyWPF/MySelf.PMS.Server/MySelf.PMS.Server.Start/Program.cs
@@ -1,25 +1,80 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using MySelf.PMS.Server.IService;
+using MySelf.PMS.Server.Service;
+using SqlSugar;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Zhaoxi.PMS.Server.Start
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //// 测试   演示基本的SqlSugar的使用
+            //string connStr = "Data Source=localhost;Initial Catalog=PMS2024;User ID=sa;Password=123456";
+            //ConnectionConfig config = new ConnectionConfig()
+            //{
+            //    DbType = SqlSugar.DbType.SqlServer,
+            //    ConnectionString = connStr,
+            //    IsAutoCloseConnection = true
+            //};
+
+            //SqlSugarClient sqlSugarClient = new SqlSugarClient(config);
+
+            ////client.Open();
+            //var es = sqlSugarClient.Queryable<SysEmployee>().ToList();
+
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            RegistarClasses(builder.Services);
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+
+        private static void RegistarClasses(IServiceCollection services)
+        {
+            services.AddTransient<ISqlSugarClient>(client =>
+            {
+                //string connStr = "Data Source=localhost;Initial Catalog=PMS2024;User ID=sa;Password=123456";
+                string connStr = "Data Source=localhost\\SQLEXPRESS01;Initial Catalog=PMS2024;Integrated Security=True;Trust Server Certificate=True";
+                //string connStr = "server=localhost;Database=PMS2024;Uid=root;Pwd=123456;charset=utf8mb4;pooling=true";
+                ConnectionConfig config = new ConnectionConfig()
+                {
+                    DbType = SqlSugar.DbType.SqlServer,
+                    //DbType = SqlSugar.DbType.MySql,
+                    ConnectionString = connStr,
+                    IsAutoCloseConnection = true
+                };
+
+                return new SqlSugarClient(config);
+            });
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IFileService, FileService>();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
