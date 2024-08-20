319b4ed99e640856066b3a7d94dd55e0e63f43f2
何正方 <hezhengfang08@163.com>
2024-08-20 17:46:00 +0800

    重新生成发布文件

--- vue32-TS/myVue-TsLearn/src/views/files.ts
+++ vue32-TS/myVue-TsLearn/src/views/files.ts
@@ -1,15 +1,15 @@
 import {IFiles} from '@/utils/dataType'
 
 //首页
-import Home from './content/Home/index.vue'
+import Home from './content/home/index.vue'
 //详情页
 import Detail from './content/detail.vue'
 //分类搜索
-import Search from './content/Search/index.vue'
+import Search from './content/search/index.vue'
 //购物车
-import Shopping from './content/Shopping/index.vue'
+import Shopping from './content/shopping/index.vue'
 //我的
-import Mine from './content/Mine/index.vue'
+import Mine from './content/mine/index.vue'
 
 
 export const Files:IFiles={
