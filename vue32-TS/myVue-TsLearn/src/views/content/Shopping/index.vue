b1c65b790aa7a12cae173fe695590e37a5f64c90
何正方 <hezhengfang08@163.com>
2024-08-20 17:13:06 +0800

    改名

--- vue32-TS/myVue-TsLearn/src/views/content/Shopping/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>{{str}}--{{num}}</div>
-</template>
-
-<script setup lang="ts">
-import {ref} from 'vue'
-const str = ref<string>('shopping');
-const num = ref<number>(0);
-</script>
